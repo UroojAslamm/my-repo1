d0949a02e53874cc05f77d083ae789e71064cecc
UroojAslamm <ut4800@g.rit.edu>
2025-09-17 13:22:31 +0400

    My first repo

--- code.py
+++ code.py
@@ -1,1 +1,85 @@
-print ("hello world")
+
+
+user_input= float(input("enter a number: "))
+user_input2= float(input("enter your second number: "))
+
+def add(user_input, user_input2):
+    
+    """
+    Description:
+    function num will add the two numbers and return them
+
+    Params: 
+    user_input this is the first number
+    user_input2 this is the second number
+
+    return:
+    sum type float will give the summation of the two numbers 
+    """
+    sum=user_input+user_input2
+    return sum
+
+
+
+
+def subtract(x,y):
+        """
+    Description:
+    function num will subtract the two numbers and return them
+
+    Params: 
+    user_input this is the first number
+    user_input2 this is the second number
+
+    return:
+    subtract type float will give the subtraction of the two numbers 
+    """
+subtract=x-y
+return subtract
+
+
+subtract = subtract(user_input,user_input2)
+print(subtract)
+
+def divide(a,b):
+
+    """
+    Description:
+    function divide will divide the two numbers and return them
+
+    Params: 
+    user_input this is the first number
+    user_input2 this is the second number
+
+    return:
+    divide type float will give the quotient of the two numbers 
+    """
+
+  
+   result= a//b
+   return result
+
+
+
+
+def main():
+
+    user_input= float(input("enter a number: "))
+    user_input2= float(input("enter your second number: "))
+
+    addition = add(user_input, user_input2)
+    print(addition)
+
+    divide1 = divide(user_input,user_input2)
+    print (divide1)
+
+  
+    subtract = subtract(user_input,user_input2)
+    print(subtract)
+
+  
+ 
+main()
+
+
+
